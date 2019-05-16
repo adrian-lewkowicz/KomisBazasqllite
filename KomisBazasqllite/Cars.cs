b82e7a81cec17c9d12086924b06f636070b61f57
Adrian Lewkowicz <adrian.lewkowicz.biala@gmail.com>
2019-05-16 23:24:42 +0200

    uzupełnienie onNavigated to i dodawanie

--- KomisBazasqllite/Cars.cs
+++ KomisBazasqllite/Cars.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,19 +9,17 @@ namespace KomisBazasqllite
 {
     class Cars
     {
-        
-        private int Id;
-        private string marka;
-        private string model;
-        private int rok;
 
         public Cars()
         {
         }
-
-        public int Id1 { get => Id; set => Id = value; }
-        public string Marka { get => marka; set => marka = value; }
-        public string Model { get => model; set => model = value; }
-        public int Rok { get => rok; set => rok = value; }
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [MaxLength(30)]
+        public string Marka { get; set; }
+        [MaxLength(30)]
+        public string Model { get; set; }
+        [MaxLength(5)]
+        public int Rok { get; set; }
     }
 }
--- KomisBazasqllite/MainPage.xaml.cs
+++ KomisBazasqllite/MainPage.xaml.cs
@@ -1,16 +1,12 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.SQLite;
 using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 //Szablon elementu Pusta strona jest udokumentowany na stronie https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x415
@@ -22,6 +18,8 @@ namespace KomisBazasqllite
     /// </summary>
     public sealed partial class MainPage : Page
     {
+
+        List<Cars> cars = new List<Cars>();
         public MainPage()
         {
             this.InitializeComponent();
@@ -29,7 +27,41 @@ namespace KomisBazasqllite
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
+            var path = Path.Combine(ApplicationData.Current.LocalFolder.Path,"cars.db");
+            var conn = new SQLiteAsyncConnection(path, true);
+            conn.CreateTableAsync<Cars>();
+            reload();
+        }
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            var path = Path.Combine(ApplicationData.Current.LocalFolder.Path, "cars.db");
+            var conn = new SQLiteAsyncConnection(path, true);
+            var car = new Cars {
+                Marka = marka.Text,
+                Model = model.Text,
+                Rok=Convert.ToInt32(rok.Text)
+            };
+            conn.InsertAsync(car);
+            reload();
+        }
+
+        private void Edit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Remove_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        async void reload()
+        {
+            var path = Path.Combine(ApplicationData.Current.LocalFolder.Path, "cars.db");
+            var conn = new SQLiteAsyncConnection(path, true);
+            var query = conn.Table<Cars>();
+            cars = await query.ToListAsync();
+            ListView.ItemsSource = cars;
         }
     }
-    
+
 }
